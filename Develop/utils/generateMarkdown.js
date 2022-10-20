77597d4160064a8232039071a19627ef21b788f8
eugene isingizwe <eugeneisingizwe95@hotmail.com>
2022-10-19 23:59:50 -0600

    updated readME file and prompts

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,6 +3,10 @@
 function renderLicenseBadge(license) {
   if (license === "No license"){
     return "";
+  } else if (license == "MIT"){
+    return ` ![MIT License](https://img.shields.io/badge/license-MIT-orange.png)`
+  } else if (license == "Unlicense"){
+    return ` ![Ulicensed](https://img.shields.io/badge/license-Unlicensed-orange.png)`
   }
   
 }
@@ -24,15 +28,54 @@ function renderLicenseSection(license) {
     return "";
   } else if (license == "MIT"){
     return `
-
+## MIT License 
 Copyright 2022
 
-![MIT License](https://img.shields.io/badge/license-MIT-orange.png)`
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE`
+
   } else if(license == "Unlicense"){
     return `
-
+## Unlicense
 Copyright 2022
-    ![Ulicensed](https://img.shields.io/badge/license-Unlicensed-orange.png)
+his is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>
     `
   } 
 }
@@ -45,7 +88,7 @@ function generateMarkdown(data) {
   ${data.Description}
 
   ## Table of Content (Optional)
-   - [Installation](#installation)
+   - [Installation](#Installation)
    - [Usage](#Usage)
    - [Credits](#Credits)
     ${renderLicenseLink(data.License)}
@@ -56,11 +99,11 @@ function generateMarkdown(data) {
   ## Usage 
   ${data.Usage}
   
-  ## credit
+  ## Credit
   ${data.Credits}
   
-  ## License
   ${renderLicenseSection(data.License)}
+  ${renderLicenseBadge(data.License)}
 
   ## Features
   ${data.Features}
@@ -71,7 +114,7 @@ function generateMarkdown(data) {
   ## Test
   ${data.Test}
 
-  ## question 
+  ## Question 
   ${data.Emailaddress}
   ${data.Githubusername}
 
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Proffesional-README-Generator-
-When creating an open source project on Github, its important to have a high-quality README for the app. This project include what the app is for, how to use the app, how to install, report issues, and make contributions. 
+# Proffesional-README-Generator
+
+## Description
+
+ - The motivation behind this project was to create a proffesional ReadME generator using commond line via Node.js. 
+ - This project was build because when creating an open source project on GitHub, it’s important to have a high-quality README for the app.
+ - A proffesional and a high-quality ReadME clarifies what the app is for, how to use the app, how to install it, how to report issues, and how to make contributions. 
+ - In this project I lerned how to create a ReadMe using commond line through Node.js. I was able to apply concerts just as template literals, write and append files, modularization, package npm, and inquirer. 
+
+
+## User Story
+
+```md
+AS A developer
+I WANT a README generator
+SO THAT I can quickly create a professional README for a new project
+```
+
+## Acceptance Criteria
+
+```md
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for information about my application repository
+THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+WHEN I enter my project title
+THEN this is displayed as the title of the README
+WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+WHEN I choose a license for my application from a list of options
+THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+WHEN I enter my GitHub username
+THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+WHEN I enter my email address
+THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+WHEN I click on the links in the Table of Contents
+THEN I am taken to the corresponding section of the README
+```
+
+
+
+---
+
+© 2022 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
